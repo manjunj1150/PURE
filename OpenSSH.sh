0913e12e85b12f382a6984e6a547abe26b7223be
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2019-05-01 09:56:42 +0700

    Update OpenSSH.sh

--- OpenSSH.sh
+++ OpenSSH.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #script OpenSSH by Pirakit Khawpleum
 
-wget -O /etc/ssh/sshd_config 'https://raw.githubusercontent.com/jiraphaty/auto-script-vpn/master/sshd_config'
+wget -O /etc/ssh/sshd_config 'https://raw.githubusercontent.com/MyGatherBk/PURE/master/sshd_config'
 
 service ssh restart
